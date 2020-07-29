439df7267db1969c6b63d68fccdc63ca9a495f95
Aaron <timbolaj@gmail.com>
2020-07-29 09:23:06 -0400

    Created table and table row components

---
+++ src/Components/Rows.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Row() {
+  return (
+    <div className="Row">
+      <table>
+        <th>Hello</th>
+        <th>Hi</th>
+      </table>
+
+    </div>
+  )
+}
