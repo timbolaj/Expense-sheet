dde5398fe87760fc1e7e26fc6c0eb26e347c6eaa
Aaron <timbolaj@gmail.com>
2020-07-29 18:50:44 -0400

    done for day

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -3,6 +3,21 @@ import Row from './Rows';
 import '../Styles/Table.scss';
 
 export default function Table() {
+
+  const addRow = () => {
+    const table = document.getElementsByTagName("table")
+    return table.push(
+      <tr>
+        <td>
+          <Row />
+        </td>
+        <td>
+          <Row />
+        </td>
+      </tr>
+    )
+  }
+
   return (
     <div className="Table">
       <table>
