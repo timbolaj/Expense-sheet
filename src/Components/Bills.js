94711739f5d31b7389c8b09a33d627f3b3d4b481
Aaron <timbolaj@gmail.com>
2020-08-22 12:11:17 -0400

    basic setup for Bills component done

--- src/Components/Bills.js
+++ src/Components/Bills.js
@@ -3,6 +3,30 @@ import Row from './Rows';
 
 export default function Bills() {
   return (
-    <h2>Bills</h2>
+    <div>
+      <h3>Bills</h3>
+      <table>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+        </tr>
+
+        <tr>
+          <td>Rent</td>
+          <Row />
+        </tr>
+
+        <tr>
+          <td>Internet</td>
+          <Row />
+        </tr>
+
+        <tr>
+          <td>Cellphone</td>
+          <Row />
+        </tr>
+
+      </table>
+    </div>
   )
 }
