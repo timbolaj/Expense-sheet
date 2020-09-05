f7ba2c7c08fac8a3c62298221fd347c8155d0a33
Aaron <timbolaj@gmail.com>
2020-09-05 11:27:06 -0400

    basic styles made for money out components

--- src/Components/Debt-Invest/Debt-invest.js
+++ src/Components/Debt-Invest/Debt-invest.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Investments from './Investments';
 import Loans from './Loans';
+import '../../Styles/Debt-invest.scss';
 
 export default function DebtInvest() {
   return (
     <div>
       <h2>Debt-Invest</h2>
-      <Loans />
-      <Investments />
+      <div className="debt-invest">
+        <Loans />
+        <Investments />        
+      </div>
+
     </div>
   )
 }
