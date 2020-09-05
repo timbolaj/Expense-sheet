1659ca336f397cd38bd25ad17562d226c44ee17f
Aaron <timbolaj@gmail.com>
2020-09-05 11:22:08 -0400

    finished investment component

--- src/Components/Debt-Invest/Investments.js
+++ src/Components/Debt-Invest/Investments.js
@@ -5,6 +5,23 @@ export default function Investments() {
   return (
     <div>
       <h2>Investments</h2>
+      <table>
+        <tr>
+          <th>Description</th>
+          <th>Amount</th>
+        </tr>
+
+        <tr>
+          <td>Short-term</td>
+          <Row />
+        </tr>
+
+        <tr>
+          <td>Long-term</td>
+          <Row />
+        </tr>
+      </table>
+    
     </div>
   )
 }
